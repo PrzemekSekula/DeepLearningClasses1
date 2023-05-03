8b791147246391863d9aceb6728646a4a80f3d97
PrzemekS <przemeksekula@gmail.com>
2023-05-03 12:16:33 +0200

    feat: Bias-variance improved

--- BiasVarianceDemo/helper.py
+++ BiasVarianceDemo/helper.py
@@ -6,3 +6,13 @@ def plot_model(model, df, x_column = 'area', y_column = 'price'):
     new_x = np.arange(min(df[x_column]), max(df[x_column]))
     new_y = model.predict(new_x.reshape(-1, 1))
     plt.plot(new_x, new_y, '-', color='red')
+    
+    
+def plot_models(models, df, x_column = 'area', y_column = 'price'):
+    plt.figure(figsize=(10, 4))
+    for i, model in enumerate(models):
+        plt.subplot(1, len(models), i+1)
+        plt.scatter(df[x_column], df[y_column], s=1)
+        new_x = np.arange(min(df[x_column]), max(df[x_column]))
+        new_y = model.predict(new_x.reshape(-1, 1))
+        plt.plot(new_x, new_y, '-', color='red')           
